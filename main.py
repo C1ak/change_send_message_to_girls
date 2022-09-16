80da689a97a4713907aff1bb6d4cc824a39eca88
C1ak <92179381+C1ak@users.noreply.github.com>
2022-09-16 17:51:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def get_counter_left(aim_date):
   else:
     print('日期格式不符合要求')
     
-  if next > nowtime:
+  if next < nowtime:
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
